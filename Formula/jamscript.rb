4b13eb6637943e10981f20b5c2b58037445a5782
arh96480 <arh96480@gmail.com>
2024-08-13 15:51:54 -0400

    Update jamscript.rb

--- Formula/jamscript.rb
+++ Formula/jamscript.rb
@@ -1,7 +1,7 @@
 class Jamscript < Formula
   desc "Jam-themed esoteric programming language interpreter"
-  homepage "https://github.com/yourusername/homebrew-jamscript"
-  url "https://github.com/yourusername/homebrew-jamscript/archive/v1.0.tar.gz"
+  homepage "https://github.com/arh96480/homebrew-jamscript"
+  url "https://github.com/arh96480/homebrew-jamscript/archive/v1.0.tar.gz"
   sha256 "653bf2cbe5985432ff37a1b7ec7f12483c41dc44ed1620de7ed93e87fc55788a"
 
   def install
